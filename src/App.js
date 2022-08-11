8d776770bf798e280b1fe0f6d292f47e2ea3eed7
jancy <jotabbusiness@icloud.com>
2022-08-11 04:34:57 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -1,63 +1,42 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { marked } from 'marked';
 
 
 function App() {
-
   const [text, setText] = useState('')
 
   const Answer = () => {
-    const answer = marked.parse(text)
-    console.log(text)
+    const answer = marked.parse(text);
     return answer
+
   }
 
-  const elements = `# Welcome to my React Markdown Previewer!!!
-
-  ## This is a sub-heading...
-  ### And here's some other cool stuff:
+  const words = `# Hello, There
+  ## Welcome to my Page
   
+  There's also [links](https://www.freecodecamp.org) to joining Free Code Camp and learning
+   
+  > These are a numbered list!
+  1. And this is insane
+  1. Its so fun!
   
-  You can also make text **bold**... whoa!
-  Or _italic_.
-  Or... wait for it... **_both!_**
-  And feel free to go crazy ~~crossing stuff out~~.
-  
-  There's also [links](https://www.freecodecamp.org), and
-  > Block Quotes!
-  
-  And if you want to get really ~~crazy, even tables~~:
-  
-  Wild Header | Crazy Header | Another Header?
-  ------------ | ------------- | -------------
-  Your content can | be here, and it | can be here....
-  And here. | Okay. | I think we get it.
-  
-  - And of course there are lists.
-    - Some are bulleted.
-       - With different indentation levels.
-          - That look like this.
-  
-  
-  1. And there are numbered lists too.
-  1. Use just 1s if you want!
-  1. And last but not least, let's not forget embedded images:
-  `
-
+  > This **word** is bold. This <em>word</em> is italic`
 
   return (
     <div className="App">
       <div>
-      <h3 className='markdown'>Markdown</h3>
+      <h3 className='title markdown'>Markdown</h3>
         <textarea
           id='textarea'
           onChange={(e) => setText(e.target.value)}
-        >{elements}
+          value={text}
+        >
+          # hello
         </textarea>
         </div>
         <div>
-      <h3 className='previewing'>Preview</h3>
+      <h3 className='bold'>Preview</h3>
       <div
         className='preview'
         dangerouslySetInnerHTML={{ __html: Answer() }}>
