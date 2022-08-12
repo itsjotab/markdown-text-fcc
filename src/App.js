26406b83d8a6043d298a1efc7e4afadaa5f58378
jancy <jotabbusiness@icloud.com>
2022-08-12 01:04:17 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { marked } from 'marked';
 
 
@@ -26,23 +26,20 @@ function App() {
   return (
     <div className="App">
       <div>
-      <h3 className='title markdown'>Markdown</h3>
+        <h3 className='title'>Markdown</h3>
         <textarea
           id='textarea'
           onChange={(e) => setText(e.target.value)}
-          value={text}
         >
-          # hello
         </textarea>
+      </div>
+      <div>
+        <h3 className='bold'>Preview</h3>
+        <div
+          className='preview'
+          dangerouslySetInnerHTML={{ __html: Answer() }}>
         </div>
-        <div>
-      <h3 className='bold'>Preview</h3>
-      <div
-        className='preview'
-        dangerouslySetInnerHTML={{ __html: Answer() }}>
       </div>
-      </div>
-      <script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js'></script>
     </div>
   );
 }
